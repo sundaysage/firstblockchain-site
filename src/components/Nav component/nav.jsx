45f69a11f4c8cef81a58a147d6967a19928247cb
sundaysage <semenike60@gmail.com>
2022-10-30 14:07:17 -0700

    firstcomms

--- src/components/Nav component/nav.jsx
+++ src/components/Nav component/nav.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import "../Nav component/nav.css";
 const Nav = () => {
   return (
-    <div>
+    <div className="mainNav">
+      <div>
+        <span className="new">New</span>
+        <span className="gen">Gen</span>
+      </div>
       <nav>
-        <div> <span className="new">New</span><span className="gen">Gen</span></div>
-        <div className="navlist2">
-          <a href=""><li>Home</li></a>
-          <a href=""><li>Market</li></a>
-          <a href=""><li>Artist</li></a>
-          <a href=""><li>Community</li></a>
-          
-        </div>
-        <div className="logupbtDiv">
-          <button className="loginbt">Login</button>
-          <button className="Signupbt">Signup</button>
+        <p className="more"id="moreid">more</p>
+        <div className="navlist2 shownavlist2" id="navlistid">
+          <a href="">
+            <li>Home</li>
+          </a>
+          <a href="">
+            <li>Market</li>
+          </a>
+          <a href="">
+            <li>Artist</li>
+          </a>
+          <a href="">
+            <li>Community</li>
+          </a>
+          <div className="logupbtDiv">
+            <button className="loginbt">Login</button>
+            <button className="Signupbt">Signup</button>
+          </div>
         </div>
       </nav>
     </div>
