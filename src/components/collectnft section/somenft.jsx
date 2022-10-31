0db0510a5e0672d061a7d5f4464f91c0dee10b3e
sundaysage <semenike60@gmail.com>
2022-10-31 16:30:43 -0700

    3comms

--- src/components/collectnft section/somenft.jsx
+++ src/components/collectnft section/somenft.jsx
@@ -4,61 +4,71 @@ import glassdata from "../assets/big-glass-data.jpg";
 
 const Somenft = () => {
   return (
-    <div  >
+    <div className="fathersomenft">
       <h1 className="collectsome">Collect Some Nfts</h1>
       <div className="nftboxDiv">
-        <div className="nftbox">
-          <img src="" alt="" />
+
+
+        <div className="nftboxing">
+          <div className="imgdiv"></div>
           <div className="nftTag">
-            <div className="namepriceTag">
-              <div>
-                <p>Glass data</p>
-                <p>SAGE</p>f
-              </div>
-              <p>0.5ETH</p>
+            <div>
+              <p>Glass data</p>
+              <p>SAGE</p>f
             </div>
+            <p>0.5ETH</p>
             <button> Place A Bid</button>
           </div>
         </div>
-        <div className="nftbox" id="nftbox2">
-          <img src="" alt="" />
+
+        <div className="nftboxing">
+          <div className="imgdiv"></div>
           <div className="nftTag">
-            <div className="namepriceTag">
-              <div>
-                <p>house data</p>
-                <p>SAGE</p>
-              </div>
-              <p>0.5ETH</p>
+            <div>
+              <p>Glass data</p>
+              <p>SAGE</p>f
             </div>
+            <p>0.5ETH</p>
             <button> Place A Bid</button>
           </div>
         </div>
-        <div className="nftbox" id="nftbox3">
-          <img src="" alt="" />
+
+        <div className="nftboxing">
+          <div className="imgdiv"></div>
           <div className="nftTag">
-            <div className="namepriceTag">
-              <div>
-                <p>Glass data</p>
-                <p>SAGE</p>
-              </div>
-              <p>0.5ETH</p>
+            <div>
+              <p>Glass data</p>
+              <p>SAGE</p>f
             </div>
+            <p>0.5ETH</p>
             <button> Place A Bid</button>
           </div>
         </div>
-        <div className="nftbox" id="nftbox4">
-          <img src="" alt="" />
+
+        <div className="nftboxing">
+          <div className="imgdiv"></div>
           <div className="nftTag">
-            <div className="namepriceTag">
-              <div>
-                <p>Digi chain</p>
-                <p>SAGE</p>
-              </div>
-              <p>0.5ETH</p>
+            <div>
+              <p>Glass data</p>
+              <p>SAGE</p>f
             </div>
+            <p>0.5ETH</p>
             <button> Place A Bid</button>
           </div>
         </div>
+
+        <div className="nftboxing">
+          <div className="imgdiv"></div>
+          <div className="nftTag">
+            <div>
+              <p>Glass data</p>
+              <p>SAGE</p>f
+            </div>
+            <p>0.5ETH</p>
+            <button> Place A Bid</button>
+          </div>
+        </div>
+       
       </div>
     </div>
   );
